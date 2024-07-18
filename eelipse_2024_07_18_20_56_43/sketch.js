a399448ddcc10b9632a42c5d06b15c0c04ee8d45
simranmani <manisimran778@gmail.com>
2024-07-19 02:28:29 +0530

    add files

---
+++ eelipse_2024_07_18_20_56_43/sketch.js
@@ -0,0 +1,15 @@
+function setup() {
+  createCanvas(600, 600,WEBGL);
+}
+
+function draw() {
+  background(119, 195, 242);
+  // Use six vertices
+  ellipse(0,0,100,200,10);
+  describe( 'a white hexagon on a blue canvas');
+  
+  ellipse(100,10,50,80)
+  fill(242, 203, 119)
+  
+}
+
